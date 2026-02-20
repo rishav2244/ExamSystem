c368d87c9ca7fe59908bfe309342bb5dff5cc357
rishav2244 <rishavganguli1@gmail.com>
2026-02-20 09:52:42 +0530

    User group documentation

--- ExamBackend/ExamBackend/src/main/java/com/company/ExamBackend/controller/UserGroupController.java
+++ ExamBackend/ExamBackend/src/main/java/com/company/ExamBackend/controller/UserGroupController.java
@@ -5,6 +5,8 @@ import com.company.ExamBackend.dto.GrpMemberDTO;
 import com.company.ExamBackend.dto.UserGroupResponseDTO;
 import com.company.ExamBackend.model.UserGroup;
 import com.company.ExamBackend.service.UserGroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +16,18 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/userGroups")
+@Tag(
+        name = "User groups management",
+        description = "Endpoints for managing user groups."
+)
 public class UserGroupController {
 
     private final UserGroupService userGroupService;
 
+    @Operation(
+            summary = "Creates a new group",
+            description = "Used by admin to create a new group."
+    )
     @PostMapping("create")
     public ResponseEntity<Void> createUser(@RequestBody CreateGroupDTO createGroupDTO) {
         userGroupService.createUserGroup(createGroupDTO);
@@ -25,15 +35,27 @@ public class UserGroupController {
     }
 
     @GetMapping("")
+    @Operation(
+            summary = "Gets all groups",
+            description = "Used by admin to create new user."
+    )
     public ResponseEntity<List<UserGroupResponseDTO>> getAllUserGroups() {
         return ResponseEntity.ok(userGroupService.getAllUserGroups());
     }
 
+    @Operation(
+            summary = "Gets list of users ina  group",
+            description = "Used by admin list users from group."
+    )
     @GetMapping("/userList/{groupId}")
     public ResponseEntity<List<GrpMemberDTO>> getAllUsersByGroupId(@PathVariable String groupId) {
         return ResponseEntity.ok(userGroupService.getMembersByGroupId(groupId));
     }
 
+    @Operation(
+            summary = "Deletes a group",
+            description = "Used by admin to delete a group."
+    )
     @DeleteMapping("/delete/{groupId}")
     public ResponseEntity<Void> deleteUserGroup(@PathVariable String groupId) {
         userGroupService.deleteUserGroup(groupId);
