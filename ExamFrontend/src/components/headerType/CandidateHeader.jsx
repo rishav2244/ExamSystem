dbcaf43cc557b28d79d2529105b536dd49ba07a8
Abhishikta <dabhishikta2002@gmail.com>
2026-02-26 11:24:29 +0530

    some css changes and code cleaning

--- ExamFrontend/src/components/headerType/CandidateHeader.jsx
+++ ExamFrontend/src/components/headerType/CandidateHeader.jsx
@@ -3,35 +3,44 @@ import { AuthenticationContext } from "../../context/AuthenticationContext";
 import { ResetPasswordModal } from "../../pages/ResetPasswordModal";
 
 export const CandidateHeader = () => {
+
     const { name, email, logout } = useContext(AuthenticationContext);
     const [showResetModal, setShowResetModal] = useState(false);
 
     return (
         <header className="admin-header">
+
             <h4 className="admin-header__welcome">
                 Welcome, <span>{name}</span>
             </h4>
 
             <div className="admin-header__nav">
-                <button 
-                    className="admin-header__link"
+
+
+                <button
+                    className="change-password-btn"
                     onClick={() => setShowResetModal(true)}
-                    style={{ background: 'none', border: 'none', color: 'inherit', cursor: 'pointer' }}
                 >
                     Change Password
                 </button>
+
             </div>
 
-            <button className="admin-header__logout" onClick={logout}>
+
+            <button
+                className="admin-header__logout"
+                onClick={logout}
+            >
                 Log out
             </button>
 
             {showResetModal && (
-                <ResetPasswordModal 
-                    email={email} 
-                    onClose={() => setShowResetModal(false)} 
+                <ResetPasswordModal
+                    email={email}
+                    onClose={() => setShowResetModal(false)}
                 />
             )}
+
         </header>
     );
 };
