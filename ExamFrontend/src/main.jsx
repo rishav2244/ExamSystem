14bcec0af2c752088741d71ebaaf15d2d54a1c0f
Abhishikta <dabhishikta2002@gmail.com>
2026-03-10 11:36:12 +0530

    Exam Modal Popup Creation

--- ExamFrontend/src/main.jsx
+++ ExamFrontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { PopupProvider } from "./components/popupType/usePopup";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PopupProvider>
+      <App />
+    </PopupProvider>
   </React.StrictMode>
 );
