3bac64caa1c35f55cfd385417d24e66a49389c19
Abhishikta <dabhishikta2002@gmail.com>
2026-03-12 11:36:02 +0530

    code cleaning

--- ExamFrontend/src/components/popupType/MailSendingModal.jsx
+++ ExamFrontend/src/components/popupType/MailSendingModal.jsx
@@ -5,13 +5,11 @@ export const MailSendingModal = () => {
     <div className="mail-sending-overlay">
       <div className="mail-sending-box">
 
-        <div className="mail-icon">
-          {/* 📧 */}
-        </div>
+        <div className="mail-icon"></div>
 
         <h3>Sending Invitations...</h3>
 
-        <p>Please wait while emails are being delivered to candidates.</p>
+        <p>Please wait while the mails are being sent to the candidates.</p>
 
         <div className="mail-progress">
           <div className="mail-progress-bar"></div>
