7aa18d6930d307c93fc9893124c90b91734102af
rishav2244 <rishavganguli1@gmail.com>
2026-05-13 12:28:38 +0530

    Removed unused import

--- ExamBackend/ExamBackend/src/main/java/com/company/ExamBackend/service/impl/EmailServiceImpl.java
+++ ExamBackend/ExamBackend/src/main/java/com/company/ExamBackend/service/impl/EmailServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
